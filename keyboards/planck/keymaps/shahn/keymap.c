0ac286feb31f90b11b1217df872c83f090877352
Sönke Hahn <soenkehahn@gmail.com>
2019-08-07 09:19:50 -0400

    switch the neo letter layout

--- keyboards/planck/keymaps/shahn/keymap.c
+++ keyboards/planck/keymaps/shahn/keymap.c
@@ -17,6 +17,8 @@
 #include QMK_KEYBOARD_H
 #include "muse.h"
 
+#include "keymap_german.h"
+
 extern keymap_config_t keymap_config;
 
 enum planck_layers {
@@ -55,10 +57,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT_planck_grid(
-    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
-    KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT ,
-    BACKLIT, KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+    KC_TAB,  KC_X,    KC_V,    KC_L,    KC_C,  KC_W,   KC_K,   KC_H,  KC_G,    KC_F,    KC_Q,    KC_BSPC,
+    KC_ESC,  KC_U,    KC_I,    KC_A,    KC_E,  KC_O,   KC_S,   KC_N,  KC_R,    KC_T,    KC_D,    KC_QUOT,
+    KC_LSFT, DE_UE,   DE_OE,   DE_AE,   KC_P,  KC_Z,   KC_B,   KC_M,  KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
+    RGB_TOG, KC_LCTL, KC_LALT, KC_LGUI, LOWER, KC_SPC, KC_SPC, RAISE, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
 ),
 
 /* Colemak
