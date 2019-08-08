4d4ebc69d3fa18b7e5d9668cf823d55082607bdb
Sönke Hahn <soenkehahn@gmail.com>
2019-08-08 10:48:26 -0400

    remove more unused layers

--- keyboards/planck/keymaps/shahn/keymap.c
+++ keyboards/planck/keymaps/shahn/keymap.c
@@ -19,9 +19,6 @@
 
 enum planck_layers {
   _QWERTY,
-  _LOWER,
-  _RAISE,
-  _ADJUST,
   LAYER_4
 };
 
@@ -30,33 +27,12 @@ enum planck_keycodes {
   BACKLIT,
 };
 
-#define LOWER MO(_LOWER)
-#define RAISE MO(_RAISE)
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_QWERTY] = LAYOUT_planck_grid(
-    KC_TAB,  KC_X,    KC_V,    KC_L,    KC_C,  KC_W,   KC_K,   KC_H,        KC_G,    KC_F,    KC_Q,    KC_BSPC,
-    KC_ESC,  KC_U,    KC_I,    KC_A,    KC_E,  KC_O,   KC_S,   KC_N,        KC_R,    KC_T,    KC_D,    KC_QUOT,
-    KC_LSFT, DE_UE,   DE_OE,   DE_AE,   KC_P,  KC_Z,   KC_B,   KC_M,        KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
-    RGB_TOG, KC_LCTL, KC_LALT, KC_LGUI, LOWER, KC_SPC, KC_SPC, MO(LAYER_4), KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
-),
-[_LOWER] = LAYOUT_planck_grid(
-    KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR,    KC_ASTR,    KC_LPRN, KC_RPRN, KC_BSPC,
-    KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS,    KC_PLUS,    KC_LCBR, KC_RCBR, KC_PIPE,
-    _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  S(KC_NUHS), S(KC_NUBS), KC_HOME, KC_END,  _______,
-    _______, _______, _______, _______, _______, _______, _______, _______,    KC_MNXT,    KC_VOLD, KC_VOLU, KC_MPLY
-),
-[_RAISE] = LAYOUT_planck_grid(
-    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
-    KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
-    _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_NUHS, KC_NUBS, KC_PGUP, KC_PGDN, _______,
-    _______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY
-),
-[_ADJUST] = LAYOUT_planck_grid(
-    _______, RESET,   DEBUG,   RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD,  RGB_VAI, RGB_VAD, KC_DEL ,
-    _______, _______, MU_MOD,  AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, QWERTY,  _______,  _______, _______, _______,
-    _______, MUV_DE,  MUV_IN,  MU_ON,   MU_OFF,  MI_ON,   MI_OFF,  TERM_ON, TERM_OFF, _______, _______, _______,
-    _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______, _______
+    KC_TAB,  KC_X,    KC_V,    KC_L,    KC_C   ,  KC_W,   KC_K,  KC_H,        KC_G,    KC_F,    KC_Q,    KC_BSPC,
+    KC_ESC,  KC_U,    KC_I,    KC_A,    KC_E   ,  KC_O,   KC_S,  KC_N,        KC_R,    KC_T,    KC_D,    KC_QUOT,
+    KC_LSFT, DE_UE,   DE_OE,   DE_AE,   KC_P   ,  KC_Z,   KC_B,  KC_M,        KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
+    RGB_TOG, KC_LCTL, KC_LALT, KC_LGUI, XXXXXXX, KC_SPC, KC_SPC, MO(LAYER_4), KC_LGUI, KC_DOWN, KC_UP,   KC_RGHT
 ),
 [LAYER_4] = LAYOUT_planck_grid(
     XXXXXXX, XXXXXXX, KC_BSPC, KC_UP,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
